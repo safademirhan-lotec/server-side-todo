2e60b40ecea8736defe190c35b38adcdc9d4ca67
Safa DEMIRHAN <safa.demirhan@lotec.com.tr>
2023-10-05 18:02:45 +0300

    Added text box for adding assignee to the todo.

--- index.js
+++ index.js
@@ -16,8 +16,6 @@ app.engine(
 );
 app.set('view engine', 'handlebars');
 
-// Middleware to parse JSON and form data
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from the "public" directory
